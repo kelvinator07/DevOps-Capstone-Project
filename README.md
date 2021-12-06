d40cdc0c7e88041c2310ec76cabbc0d36c39ce69
Kelvin Isievwore <kelvinator4leo@gmail.com>
2021-12-06 02:21:56 +0100

    first-commit

---
+++ README.md
@@ -0,0 +1,3 @@
+[![CircleCI](https://circleci.com/gh/kelvinator07/DevOps-Capstone-Project/tree/master.svg?style=svg)](https://circleci.com/gh/kelvinator07/DevOps-Capstone-Project/tree/master)
+
+# Devops-Capstone-Project
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>AWS Cloud Devops</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
